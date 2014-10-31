9a7f0be8d64f2b9738ff5c9ba4406666734f3591
Austin Bourgerie <bourgerie.a@gmail.com>
2014-10-30 21:52:10 -0400

    Added support for engagement tracking

--- src/spawnutility/SpawnCommandExecutor.java
+++ src/spawnutility/SpawnCommandExecutor.java
@@ -6,17 +6,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import core.EngagementTracker.PlayerEngageListener;
 import core.Utilities.LocationSelector;
 
 public class SpawnCommandExecutor implements CommandExecutor
 {
 	private SpawnUtility plugin;
-	
+
 	public SpawnCommandExecutor(SpawnUtility plugin)
 	{
 		this.plugin = plugin;
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command command, String commandName, String[] args)
 	{
 		if(!(sender instanceof Player))
@@ -24,13 +25,20 @@ public class SpawnCommandExecutor implements CommandExecutor
 			sender.sendMessage(ChatColor.RED + "Only player can use spawn commands.");
 			return true;
 		}
-			
+
 		Player player = (Player) sender;
 
 		//If the player is going to spawn
 		if(commandName.equalsIgnoreCase("spawn"))
 		{
-			plugin.teleportToSpawn(player);
+			if (!PlayerEngageListener.isEngaged(player.getUniqueId()))
+				plugin.teleportToSpawn(player);
+			else
+				sender.sendMessage(ChatColor.RED + "You cannot use " +
+						ChatColor.GOLD + "/spawn " +
+						ChatColor.RED + "for another " +
+						ChatColor.GREEN + PlayerEngageListener.getSecondsToDisengage(player.getUniqueId()) + 
+						ChatColor.RED + " seconds!");
 			return true;
 		}
 		//If the player is trying to set spawn
@@ -43,10 +51,10 @@ public class SpawnCommandExecutor implements CommandExecutor
 			}
 			else
 				player.sendMessage(ChatColor.RED + "Please select a location with a stick.");
-			
+
 			return true;
 		}
-		
+
 		return false;
 	}
 }
